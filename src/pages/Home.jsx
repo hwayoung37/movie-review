414b33ec6b9aec25a94516b8b262cfb83ca29147
hwayoung <hyriver37@gamil.com>
2023-11-10 14:45:36 +0900

    [Fix] 장르 id 변경

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -11,19 +11,19 @@ export default function Home() {
       <MovieList />
       <h1 className="listTitle">장르별 영화</h1>
       <MovieGenresList
-        genreIds="7b7fe27c-c6e5-491d-967c-fccff40d7f81"
+        genreIds="a2052c85-dbe5-4a5d-9906-2fad41208425"
         categoryTitle="영원한 사랑 이야기: 로맨스의 꿈들이 엮이는 곳"
       />
       <MovieGenresList
-        genreIds="921117da-f038-486e-baa8-32e1bcc356e6"
+        genreIds="bb16175d-74a5-4f4e-a14b-129ba18faded"
         categoryTitle="화면을 불태우다. 폭발적인 액션, 잊지 못할 기억"
       />
       <MovieGenresList
-        genreIds="9d70f853-e452-455a-aafd-a8f12dd2bbc8"
+        genreIds="32a37731-41f5-46b6-b1be-d7eefe9e15fa"
         categoryTitle="웃음이 터진다: 코미디의 경계 없는 매력"
       />
       <MovieGenresList
-        genreIds="0c3670aa-c64a-4b95-87e9-9237db111a96"
+        genreIds="f1d79c81-3165-4971-ad18-833c042b3f24"
         categoryTitle="상상력 그 이상: 현재의 범위를 넘어선 판타지를 경험하세요"
       />
     </div>
