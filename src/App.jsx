02c2bd5c684e408693436170d239da0fb6f25224
hwayoung <hyriver37@gamil.com>
2023-11-17 13:45:28 +0900

    [Fix] 로그인 시 새로고침 되는 것 수정

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,8 @@
-import "./App.css";
-import "./Login.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import React, { useState } from "react";
+
 import Header from "./component/Header";
 import Footer from "./component/Footer";
-import MyPage from "./pages/MyPage";
 
 //pages
 import Home from "./pages/Home";
@@ -11,22 +10,33 @@ import Login from "./pages/Login";
 import MovieDetail from "./pages/MovieDetail";
 import Search from "./pages/Search";
 import Signup from "./pages/Signup";
+import MyPage from "./pages/MyPage";
 
 import ScrollToTop from "./utils/ScrollToTop";
+import "./App.css";
 
 export default function App() {
+  //accessToken으로 로그인 확인
+  const [accessToken, setAccessToken] = useState(
+    localStorage.getItem("accessToken")
+  );
+  //새로고침 시 header의 accessToken이 undefined가 되는데, 이것은 리액트를 새로고침하면 모든것이 초기상태로 돌아가기 때문이다.
+  //초기값을 미리 로컬스토리지에서 가져온 값으로 설정하면 새로고침했을 때도 로컬스토리지의 값을 가지고 올 수 있다.
+
   return (
     <BrowserRouter>
       <ScrollToTop />
-      {/* 컴포넌트이기때문에 열린태그/닫힘태그 사용안됨 */}
       <div className="app">
-        <Header />
+        <Header accessToken={accessToken} setAccessToken={setAccessToken} />
         <main className="main">
           <Routes>
             <Route path="/" element={<Home />} />
             {/* movieId 라는 파라미터를 활용해 url을 설정, 접근할 것 */}
             <Route path="/movie/:movieId" element={<MovieDetail />} />
-            <Route path="/login" element={<Login />} />
+            <Route
+              path="/login"
+              element={<Login setAccessToken={setAccessToken} />}
+            />
             <Route path="/signup" element={<Signup />} />
             {/* searchTerm 이라는 파라미터를 활용해 url을 설정, 접근할 것 */}
             <Route path="/search/:searchTerm" element={<Search />} />
--- src/component/Header.jsx
+++ src/component/Header.jsx
@@ -1,32 +1,12 @@
-import { Link, useNavigate } from "react-router-dom";
-import React, { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
+import React, { useState } from "react";
 import "../style/header.css";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faMagnifyingGlass } from "@fortawesome/free-solid-svg-icons";
 
 // 로고, 검색창, 로그인, 회원가입
-export default function Header() {
-  // useEffect(() => {
-  //     if (localStorage.getItem('accessToken') === null) {
-  //       window.location.replace('http://localhost:3000/')
-  //     }
-  //   }, []);
-
-  //  const [accessToken, setAccessToken] = useState();
-  // let accessToken = localStorage.getItem('accessToken');
-  //   useEffect(() => {
-  //    setAccessToken(localStorage.getItem('accessToken'))
-  //     console.log(accessToken);
-
-  // }, [accessToken])
-
-  // console.log(accessToken);
-  const navigate = useNavigate();
-
+export default function Header({ accessToken, setAccessToken }) {
   const [searchTerm, setSearchTerm] = useState("");
-  const [accessToken, setAccessToken] = useState(
-    localStorage.getItem("accessToken")
-  );
 
   const handleInputChange = (e) => {
     setSearchTerm(e.target.value);
@@ -36,18 +16,8 @@ export default function Header() {
     // 로그아웃 클릭 시 accessToken 제거
     localStorage.removeItem("accessToken");
     setAccessToken(false);
+    alert("로그아웃 되었습니다.");
   };
-  //이게 아닌가??? -> 아닌걸로..ㅎ
-  // const [searchUrl, setSearchUrl] = useState(``)
-  // const handleUrlChange = () => {
-  //   setSearchUrl(`/search/${searchTerm}`)
-  // }
-
-  //이거 맞는거 같은데 로직이 안떠오른다 으아 머리터져ㅎ...
-  //여기가 아니라 서치에서 했어야 함ㅇㅇ
-  // useEffect(()=> {
-
-  // }, [searchUrl])
 
   return (
     <div className="header">
@@ -67,11 +37,6 @@ export default function Header() {
             value={searchTerm}
             onChange={handleInputChange}
           />
-          {/* 
-          input의 value가 변경되면 onChange에 연결된 handleInputChange 함수가 수행되고
-          setSearchTerm을 이용해서 searchTerm이 변경된다.
-          아이콘을 클릭하면 변경된 searchTerm 값을 받아서 url이 변경되면서 해당 url로 이동한다.
-          */}
           <Link
             to={`/search/${searchTerm}`}
             type="submit"
